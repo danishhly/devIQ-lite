7c9d63dca0dcad8cb3f876f86aab573301f92382
Daan153 <creative.in47@gmail.com>
2025-11-30 15:49:17 +0530

    feat: add stats overview widget

---
+++ components/Stats.js
@@ -0,0 +1,13 @@
+import Card from "./Card";
+
+export default function Stats() {
+  return (
+    <Card title="Overview">
+      <ul>
+        <li>Users: 12</li>
+        <li>Projects: 4</li>
+        <li>Events: 245</li>
+      </ul>
+    </Card>
+  );
+}
