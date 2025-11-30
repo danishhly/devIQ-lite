7ba508baf350e80b284f2b08c22701454920e2fd
Daan153 <creative.in47@gmail.com>
2025-11-30 15:54:34 +0530

    feat: add chart placeholder component

---
+++ components/ChartPlaceHolder.js
@@ -0,0 +1,9 @@
+import Card from "./Card";
+
+export default function ChartPlaceholder() {
+  return (
+    <Card title="Traffic Chart">
+      <div className="h-40 bg-gray-200 animate-pulse rounded"></div>
+    </Card>
+  );
+}
