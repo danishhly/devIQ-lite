25792e62521124b444e73284e00a2ce856b1cb3b
Daan153 <creative.in47@gmail.com>
2025-11-30 15:51:17 +0530

    feat: add projects route and UI

---
+++ app/projects/page.js
@@ -0,0 +1,10 @@
+import ProjectList from "../../components/ProjectList";
+
+export default function ProjectsPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Projects</h1>
+      <ProjectList />
+    </div>
+  );
+}
