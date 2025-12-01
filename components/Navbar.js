b4078942253b43e5b22e5ba8aaa6cf4bf4ca9207
Daan153 <creative.in47@gmail.com>
2025-12-01 11:25:43 +0530

    feat: add navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav style={{ padding: 20, borderBottom: "1px solid #ddd" }}>
+      <a href="/">Home</a> | <a href="/tasks">Tasks</a> | <a href="/notes">Notes</a> | <a href="/habits">Habits</a>
+    </nav>
+  );
+}
