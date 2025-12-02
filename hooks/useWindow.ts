a8397e221e86a73880b576c29e4ab4d994f76143
Daan153 <creative.in47@gmail.com>
2025-12-02 18:55:32 +0530

    feat: add useWindow custom hook

---
+++ hooks/useWindow.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export function useWindow() {
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    const update = () => setWidth(window.innerWidth);
+    update();
+    window.addEventListener("resize", update);
+    return () => window.removeEventListener("resize", update);
+  }, []);
+  return width;
+}
