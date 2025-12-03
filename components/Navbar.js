c1b03e2d71292f04de975bce5a8de00e5195145a
Daan153 <creative.in47@gmail.com>
2025-12-03 09:39:37 +0530

    feat: add utilities link to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,6 +2,8 @@ export default function Navbar() {
   return (
     <nav style={{ padding: 20, borderBottom: "1px solid #ddd" }}>
       <a href="/">Home</a> | <a href="/tasks">Tasks</a> | <a href="/notes">Notes</a> | <a href="/habits">Habits</a>
+      <Link href="/utilities">Utilities</Link>
+
     </nav>
   );
 }
