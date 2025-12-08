5ab1a65a9123cb1c66cb98f3df03cfae23df3da0
Daan153 <creative.in47@gmail.com>
2025-12-08 11:29:55 +0530

    chore: add sample todo data file

---
+++ sample-todos.ts
@@ -0,0 +1,5 @@
+export const sampleTodos = [
+  { id: 1, text: "Drink water", completed: false },
+  { id: 2, text: "Study 30 mins", completed: true },
+  { id: 3, text: "Write one commit", completed: false },
+];
