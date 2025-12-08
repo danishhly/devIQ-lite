1673d7f65204f9d612a996ffb7c2cd597f30c9a5
Daan153 <creative.in47@gmail.com>
2025-12-08 11:19:35 +0530

    feat: add header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,10 +4,12 @@ import { siteConfig } from "@/lib/siteConfig";
 export default function Header() {
   return (
     <header className="py-4 border-b">
+       Productivity Dashboard
       <nav className="flex gap-4">
         <Link href="/">{siteConfig.name}</Link>
         <Link href="/about">About</Link>
       </nav>
     </header>
+    
   );
 }
