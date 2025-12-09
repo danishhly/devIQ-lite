723d8648866e3f6017836d02d303aa97bd16caf2
Daan153 <creative.in47@gmail.com>
2025-12-09 12:24:13 +0530

    style(ui): convert HabitCard inline styles to Tailwind CSS

--- components/HabitCard.js
+++ components/HabitCard.js
@@ -1,3 +1,3 @@
 export default function HabitCard({ habit }) {
-  return <div style={{ padding: 10, border: "1px solid purple" }}>{habit}</div>;
+  return <div className="p-3 border border-purple-500 rounded-md bg-purple-50">{habit}</div>;
 }
