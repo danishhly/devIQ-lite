eda5ac514ec4cd465aafc1036f7c20c0c3c94d8e
Daan153 <creative.in47@gmail.com>
2025-12-10 13:24:06 +0530

    convert HabitCard to tailwind css

--- components/HabitCard.js
+++ components/HabitCard.js
@@ -1,3 +1,3 @@
 export default function HabitCard({ habit }) {
-  return <div className="p-3 border border-purple-500 rounded-md bg-purple-50">{habit}</div>;
+  return <div className="p-3 border border-purple-500 rounded-md bg-purple-50 text-purple-800">{habit}</div>;
 }
