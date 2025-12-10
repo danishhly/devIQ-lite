2c94ff0616aacc823b1e4f3e229b1eef247d7dde
Daan153 <creative.in47@gmail.com>
2025-12-10 13:29:34 +0530

    add border decoration to SectionTitle

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -1,3 +1,3 @@
-export default function SectionTitle({ title }) {
-  return <h2 className="text-2xl font-bold mb-4">{title}</h2>;
+export default function SectionTitle({ title }: { title: string }) {
+  return <h2 className="text-2xl font-bold mb-4 border-b pb-2 border-gray-200">{title}</h2>;
 }
