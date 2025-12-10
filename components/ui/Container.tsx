e11a7c2cfed53f1797ee256328f5989f59009266
Daan153 <creative.in47@gmail.com>
2025-12-10 13:36:01 +0530

    add proper types to Container component props

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -1,3 +1,5 @@
-export default function Container({ children }) {
+import { ReactNode } from "react";
+
+export default function Container({ children }: { children: ReactNode }) {
   return <div className="max-w-5xl mx-auto px-4">{children}</div>;
 }
