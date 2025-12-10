cb03d5acf06215b8785428c4780d66d7a62d738b
Daan153 <creative.in47@gmail.com>
2025-12-10 13:22:45 +0530

    use Next.js Link for client-side routing

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,8 @@
-import Link from "next/link"; // Add import
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="p-5 border-b border-gray-200 flex gap-4">
+    <nav className="p-5 border-b border-gray-200 flex gap-4 text-blue-600">
       <Link href="/">Home</Link>
       <Link href="/tasks">Tasks</Link>
       <Link href="/notes">Notes</Link>
