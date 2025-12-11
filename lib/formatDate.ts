32afc4bb708ee778a22972384e3d5b9e8fe044d8
Daan153 <creative.in47@gmail.com>
2025-12-11 11:46:52 +0530

    support time formatting in date utility

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,7 +1,8 @@
-export function formatDate(date: Date) {
+export function formatDate(date: Date, includeTime = false) {
   return date.toLocaleDateString("en-IN", {
     day: "2-digit",
     month: "short",
     year: "numeric",
+    ...(includeTime && { hour: "2-digit", minute: "2-digit" }),
   });
 }
